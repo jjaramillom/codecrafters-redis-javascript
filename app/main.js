b2c97d23cff5f039bf1f79f028688206550384bb
Jacobo Jaramillo <jacobo.jaramillo.molina@device-insight.com>
2022-12-14 17:57:50 -0500

    step 2

--- app/main.js
+++ app/main.js
@@ -1,11 +1,13 @@
-const net = require("net");
+const net = require('net');
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+console.log('Logs from your program will appear here!');
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
+
+  connection.write('+PONG\r\n');
 });
 
-server.listen(6379, "127.0.0.1");
+server.listen(6379, '127.0.0.1');
