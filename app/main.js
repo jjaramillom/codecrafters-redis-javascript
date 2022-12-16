92d21955bedc9319117917f03f86d7a4ea5819d4
Jacobo Jaramillo <jacobo.jaramillo.molina@device-insight.com>
2022-12-16 08:27:18 -0500

    step 3

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ console.log('Logs from your program will appear here!');
 const server = net.createServer((connection) => {
   // Handle connection
 
-  connection.write('+PONG\r\n');
+  connection.on('data', () => connection.write('+PONG\r\n'));
 });
 
 server.listen(6379, '127.0.0.1');
