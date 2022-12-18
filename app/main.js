e3335728e7e72d943ba7eb68a35eaea5a98f2f32
Jacobo Jaramillo <jacobo.jaramillo.molina@device-insight.com>
2022-12-17 20:04:29 -0500

    step 4

--- app/main.js
+++ app/main.js
@@ -1,14 +1,20 @@
 const net = require('net');
+const commandRegex = /(?:\*2\r\n\$4\r\n)(\w*)(?:\r\n\$\d+\r\n)(\w*)/;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log('Logs from your program will appear here!');
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
 
-  connection.on('data', () => connection.write('+PONG\r\n'));
+  connection.on('data', (data) => {
+    const echoRegexResult = commandRegex.exec(data.toString());
+    if (echoRegexResult && echoRegexResult[1] === 'echo') {
+      connection.write(`+${echoRegexResult[2]}\r\n`);
+    } else {
+      connection.write('+PONG\r\n');
+    }
+  });
 });
 
 server.listen(6379, '127.0.0.1');
- 
